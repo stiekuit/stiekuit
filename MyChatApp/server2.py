ffedb0a7e70496040c537afea585634d5f160112
romanobrooks <romanobrooks@icloud.com>
2023-08-23 18:30:58 +0200

    removed api keys

--- MyChatApp/server2.py
+++ MyChatApp/server2.py
@@ -1,8 +1,14 @@
-import http.server
-import socketserver
-import json
 import gradio as gr
 import openai
+import mysql.connector
+
+# Set up MySQL connection
+db = mysql.connector.connect(
+    host='127.0.0.1',
+    user='dbuser',
+    password='Tesst123$$',
+    database='users'
+)
 
 # Set your OpenAI API key
 OPENAI_API_KEY = "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"
@@ -19,6 +25,21 @@ def openai_chatbot(user_input):
 
 # Define the Gradio prediction function
 def predict_chatbot(input_text):
+    # If input_text is a command, handle user registration
+    if input_text.lower() == "/register":
+        return "Please provide a username and password in the format '/register username password'."
+
+    # Process user registration
+    if input_text.lower().startswith("/register"):
+        _, username, password = input_text.split()
+        
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO Users (Username, Password) VALUES (%s, %s)", (username, password))
+        db.commit()
+
+        return f"User {username} registered successfully!"
+
+    # If not a command, proceed with chatbot interaction
     response = openai_chatbot(input_text)
     return response
 
@@ -32,3 +53,4 @@ iface = gr.Interface(
 
 # Launch the Gradio interface
 iface.launch()
+
